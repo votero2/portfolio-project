5300087cba2e1736edf36ddc58903552ed8d6b27
Vladimir Otero <votero2@yahoo.es>
2022-04-09 16:22:17 -0400

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: "#f9ab00",
+            },
+            links: {
+              enable: true,
+              color: "#f9ab00",
+              distance: 150,
+            },
+            move: {
+              enable: true,
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+                opacity: 0.5,
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>Software development and Software Apps promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Software Development",
+            "Creating",
+            "Designing",
+            "Deploying",
+            "Supporting Software",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          contact me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
